0fec64584eb8e22008fde673d14c8a7d6de02577
jose angel <joseangelespinozamorales@gmail.com>
2024-10-31 17:50:00 -0500

    MENSAJITO

---
+++ C#/WebEmpleadoCrud/WebEmpleadoCrud/WebEmpleadoCrud/Models/Combo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebEmpleadoCrud.Models
+{
+    public class Combo
+    {
+
+        public string indicador { get; set; }
+        public int combo_id {  get; set; }
+        public string combo_name { get; set; }
+
+    }
+}
---
+++ C#/WebEmpleadoCrud/WebEmpleadoCrud/WebEmpleadoCrud/servicio/ServicioCombo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebEmpleadoCrud.dao;
+using WebEmpleadoCrud.dao.daoImpl;
+using WebEmpleadoCrud.Models;
+
+namespace WebEmpleadoCrud.servicio
+{
+    public class ServicioCombo
+    {
+        public List<Combo> listadoCombo(string indicador)
+        {
+            FormularioDao dao = new FormularioDaoImpl();
+            return dao.listadoCombo(indicador);
+        }
+    }
+}
