4cf4c3344ee22c35c99eb55602173dcb6d9237e1
jose angel <joseangelespinozamorales@gmail.com>
2025-05-04 15:12:55 -0500

    MENSAJITO

---
+++ C#/Ciclo4/ExamenFINAL/BackendVenta/Infraestructura/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructura
+{
+    public class Conexion
+    {
+        private readonly string _conexion;
+
+        public Conexion(string conexion)
+        {
+            _conexion = conexion;
+        }
+    }
+}
